f04b1b0d766a598c8b5503d99a9848461a3db22a
SUHYEOK97 <rlatngur10@naver.com>
2021-05-24 17:17:33 +0900

    0524 웹 크롤링

--- 0521_Final_PJT/src/main/java/com/ssafy/happyHouse/controller/HouseController.java
+++ 0521_Final_PJT/src/main/java/com/ssafy/happyHouse/controller/HouseController.java
@@ -1,10 +1,19 @@
 package com.ssafy.happyHouse.controller;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,33 +24,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.happyHouse.model.house.DongDto;
 import com.ssafy.happyHouse.model.house.HouseDto;
 import com.ssafy.happyHouse.model.house.service.HouseService;
 
-
 @Controller
 @CrossOrigin("*")
 public class HouseController {
-	
+
 	@Autowired
 	HouseService houseService;
-	
-	@RequestMapping(value = "/houseInfo/{dong}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/houseInfo/{dong}", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<List<HouseDto>> houseInfo(@PathVariable String dong) {
 		List<HouseDto> list = houseService.getList(dong);
 		return new ResponseEntity<List<HouseDto>>(list, HttpStatus.OK);
 	}
-	
-	@RequestMapping(value = "/houseInfo/detail/{no}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/houseInfo/detail/{no}", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<HouseDto> houseDetail(@PathVariable String no) {
 		HouseDto house = houseService.getDetail(Integer.parseInt(no));
 		return new ResponseEntity<HouseDto>(house, HttpStatus.OK);
 	}
-	
-	@RequestMapping(value = "/houseInfo/apt", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/houseInfo/apt", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseEntity<List<HouseDto>> houseAptDetail(@RequestBody HouseDto param) {
 		Map<String, String> params = new HashMap<>();
@@ -50,12 +59,20 @@ public class HouseController {
 		List<HouseDto> list = houseService.getAptDetail(params);
 		return new ResponseEntity<List<HouseDto>>(list, HttpStatus.OK);
 	}
-	
-	@RequestMapping(value = "/dong", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/dong", method = RequestMethod.GET)
 	@ResponseBody
 	public ResponseEntity<List<DongDto>> getDong() {
 		List<DongDto> list = houseService.getDong();
 		return new ResponseEntity<List<DongDto>>(list, HttpStatus.OK);
 	}
-	
+
+	@RequestMapping(value = "/getNews", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<JSONObject> getNews() throws UnsupportedEncodingException, IOException, ParseException {
+		BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/newsJson.json"));
+		JSONObject json = (JSONObject) new JSONParser().parse(reader);
+		return new ResponseEntity<JSONObject>(json, HttpStatus.OK);
+	}
+
 }
---
+++ 0521_Final_PJT/src/main/java/com/ssafy/happyHouse/util/Crawler.java
@@ -0,0 +1,69 @@
+package com.ssafy.happyHouse.util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.Connection;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Crawler {
+//   @Scheduled(cron = "0 * 9 * * ?")
+	@Scheduled(fixedDelay = 5000)
+	public void cronJobSch() {
+		String URL = "https://www.google.com/search?q=%EC%95%84%ED%8C%8C%ED%8A%B8&tbm=nws";
+
+		try {
+			// 2. Connection 생성
+			Connection conn = Jsoup.connect(URL);
+
+			// 3. HTML 파싱.
+			Document html = conn.get(); // conn.post();
+
+			// 4. 요소 탐색
+			Elements names = html.getElementsByClass("JheGif nDgy9d");
+			ArrayList<String> nameList = new ArrayList<>();
+			for(Element name : names) {
+				nameList.add(name.text());
+			}
+			
+			Elements dbsr = html.getElementsByClass("dbsr");
+			ArrayList<String> hrefList = new ArrayList<>();
+			for(Element text : dbsr) {
+				Elements files = text.getElementsByTag("a");
+				String href = files.attr("href");
+				hrefList.add(href);
+			}
+			JSONObject data = new JSONObject();
+	        JSONArray req_array = new JSONArray();
+			for(int i=0; i<(nameList.size() < 6 ? nameList.size() : 6); i++) {
+				JSONObject jsonObject = new JSONObject();
+				jsonObject.put("newsName", nameList.get(i));
+				jsonObject.put("newsHref", hrefList.get(i));
+				req_array.add(jsonObject);
+			}
+			data.put("newsList", req_array);
+	        String jsonInfo = data.toJSONString();
+	    	try {
+	    		FileWriter file = new FileWriter("src/main/resources/newsJson.json");
+	    		file.write(data.toJSONString());
+	    		file.flush();
+	    		file.close();
+	     
+	    	} catch (IOException e) {
+	    		e.printStackTrace();
+	    	}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
